01643ee125159a37445ef24b30622735ae792509
Bakodev <musaabdulkabeer19@gmail.com>
2023-12-08 16:39:19 +0100

    fix: cors error

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const newsRoute = require('./routes/news');
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL);
+
+app.use(
+     cors({
+          origin: '*',
+     })
+);
 
 app.use(express.json());
 app.use(express.static('/public'));
